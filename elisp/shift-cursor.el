54eaa9c3e8f9f6d253ad006c489e028f27ec02f4
NeoISE <NeoISE@users.noreply.github.com>
2019-01-19 01:27:46 -0600

    updated and fixed some issues in shift-cursor

--- elisp/shift-cursor.el
+++ elisp/shift-cursor.el
@@ -3,11 +3,11 @@
 ;; Orig. Author:
 ;;     Name: Maniroth Ouk
 ;;     Email: maniroth_ouk@outlook.com
-;; Last Updated: <12 Jan. 2019 -- 01:48 (Central Standard Time) by Maniroth Ouk>
+;; Last Updated: <19 Jan. 2019 -- 01:18 (Central Standard Time) by Maniroth Ouk>
 ;; License: MIT
 ;;
 ;;; Commentary:
-;; Originally this minor-mode did what most editors do when \\[overwrite-mode] is active, which is to change the cursor to `bar' when in ``insert mode'' and to `box' when in ``overwrite mode''. Now, this minor-mode has been abstracted. This abstraction will allow one to change the cursor when user specific predicates (conditions) are satistied.
+;; Originally this minor-mode did what most editors do when `overwrite-mode' is active, which is to change the cursor to `bar' when in ``insert mode'' and to `box' when in ``overwrite mode''. Now, this minor-mode has been abstracted. This abstraction will allow one to change the cursor when user specific predicates (conditions) are satistied.
 ;;
 ;;; Original Commentary:
 ;; This was my first minor mode that I created. The purpose was to be able to
@@ -17,8 +17,9 @@
 ;;; Code:
 
 (defgroup shift-cursor nil
-  "Shift-Cursor mode options."
-  :group 'faces)
+  "Changes the cursor when certain conditions are met."
+  :version "25.1"
+  :group 'cursor)
 
 (defcustom shift-cursor/insert-cursor 'bar
   "The cursor that is used when the user is in `insert-mode'.
@@ -41,7 +42,7 @@ This customization variable is obsolete and one should customize the variable `s
   :group 'shift-cursor)
 
 (defcustom shift-cursor/predicate-cursor-alist '((overwrite-mode . box))
-  "This association list variable controls what cursor to be used when \\[shift-cursor-mode], or the global variant \\[global-shift-cursor-mode], is active.
+  "This association list variable controls what cursor to be used when `shift-cursor-mode', or the global variant `global-shift-cursor-mode', is active.
 
 Each value of this association list is of the form (PREDICATE . CURSOR-TYPE), PREDICATE is a condition that is tested and must be non-nil before CURSOR-TYPE is selected as the cursor type.
 If PREDICATE evaluates to nil, then the next cons cell is analyzed until the a PREDICATE evaluates to a non-nil value or all cons cells are exhausted without a single non-nil PREDICATE.
@@ -49,6 +50,7 @@ If the association list is exhausted without a non-nil PREDICATE, then the value
 
 The CURSOR-TYPE part of the cons cells should be valid value for the variable `cursor-type'."
   :type 'alist
+  :version "26.1"
   :group 'shift-cursor)
 
 (defcustom shift-cursor/default-cursor 'bar
@@ -60,12 +62,13 @@ The value of this variable must also be a valid value for the variable `cursor-t
                 (const :tag "Filled Box" :value box)
                 (const :tag "Hollow Box" :value hollow)
                 (const :tag "Vertical Bar" :value bar)
-                (cons (symbol :tag "Vertical Bar with custom width" :value bar)
-                      (integer :tag "Width"))
+                (cons :tag "Vertical Bar with custom width"
+                      (const bar) (integer :tag "Width"))
                 (const :tag "Horizontal Bar" :value hbar)
-                (cons (symbol :tag "Horizontal Bar with custom height" :value hbar)
-                      (integer :tag "Height"))
+                (cons :tag "Horizontal Bar with custom height"
+                      (const hbar) (integer :tag "Height"))
                 (other :tag "Anything Else" foo))
+  :version "26.1"
   :group 'shift-cursor)
 
 (make-obsolete-variable 'shift-cursor/insert-cursor
@@ -75,14 +78,14 @@ The value of this variable must also be a valid value for the variable `cursor-t
                         'shift-cursor/predicate-cursor-alist
                         "26.1")
 
-(make-local-variable 'shift-cursor/insert-cursor)
-(make-local-variable 'shift-cursor/overwrite-cursor)
-(make-local-variable 'shift-cursor/predicate-cursor-alist)
-(make-local-variable 'shift-cursor/default-cursor)
+(make-variable-buffer-local 'shift-cursor/insert-cursor)
+(make-variable-buffer-local 'shift-cursor/overwrite-cursor)
+(make-variable-buffer-local 'shift-cursor/predicate-cursor-alist)
+(make-variable-buffer-local 'shift-cursor/default-cursor)
 
 ;; private variables
 (defvar-local shift-cursor/old-cursor-type nil
-  "Holds the value for `cursor-type' when \\[shift-cursor-mode] is active.")
+  "Holds the value for `cursor-type' when `shift-cursor-mode' is active.")
 
 (defun shift-cursor/maybe-change-cursor nil
   "Possibly change the current cursor, if the right mode is active."
@@ -100,9 +103,9 @@ The value of this variable must also be a valid value for the variable `cursor-t
 (defun shift-cursor/maybe-change-cursor-legacy nil
   "Possibly change the current cursor, if the right mode is active.
 
-This is the legacy method of \\[shift-cursor-mode]: we are only updating the cursor to type `shift-cursor/overwrite-cursor' when \\[overwrite-mode] is active, otherwise, the cursor is of type `shift-cursor/insert-cursor'.
+This is the legacy method of `shift-cursor-mode': we are only updating the cursor to type `shift-cursor/overwrite-cursor' when `overwrite-mode' is active, otherwise, the cursor is of type `shift-cursor/insert-cursor'.
 
-There are still some issues with the abstraction of \\[shift-cursor-mode], so this legacy method is preferred while the abstraction of the minor mode is worked on."
+There are still some issues with the abstraction of `shift-cursor-mode', so this legacy method is preferred while the abstraction of the minor mode is worked on."
   (if overwrite-mode
       (setq cursor-type shift-cursor/overwrite-cursor)
     (setq cursor-type shift-cursor/insert-cursor)))
@@ -113,13 +116,16 @@ There are still some issues with the abstraction of \\[shift-cursor-mode], so th
 
 The conditions (predicates) and cursors that would activate with those predicates are configured in `shift-cursor/predicate-cursor-alist'."
   :init nil
-  :lighter " ShC"
+  :lighter " SC"
   :global nil
   :group 'shift-cursor
 
   ;; if the mode is called, toggle
   (if shift-cursor-mode
       (progn
+        (when shift-cursor-legacy-mode
+          (shift-cursor-legacy-mode -1))
+
         ;; save it to be restored
         (setq shift-cursor/old-cursor-type cursor-type)
         (add-hook 'post-command-hook #'shift-cursor/maybe-change-cursor nil t)
@@ -130,16 +136,19 @@ The conditions (predicates) and cursors that would activate with those predicate
 
 ;;;###autoload
 (define-minor-mode shift-cursor-legacy-mode
-  "This is the legacy method of \\[shift-cursor-mode]: we are only updating the cursor to type `shift-cursor/overwrite-cursor' when \\[overwrite-mode] is active, otherwise, the cursor is of type `shift-cursor/insert-cursor'.
+  "This is the legacy method of `shift-cursor-mode': we are only updating the cursor to type `shift-cursor/overwrite-cursor' when `overwrite-mode' is active, otherwise, the cursor is of type `shift-cursor/insert-cursor'.
 
-There are still some issues with the abstraction of \\[shift-cursor-mode], so this legacy method is preferred while the abstraction of the minor mode is worked on."
+There are still some issues with the abstraction of `shift-cursor-mode', so this legacy method is preferred while the abstraction of the minor mode is worked on."
   :init nil
-  :lighter " ShC"
+  :lighter " lSC"
   :global nil
   :group 'shift-cursor
 
   (if shift-cursor-legacy-mode
       (progn
+        (when shift-cursor-mode
+          (shift-cursor-mode -1))
+
         (setq shift-cursor/old-cursor-type cursor-type)
         (add-hook 'overwrite-mode-hook #'shift-cursor/maybe-change-cursor-legacy)
         (shift-cursor/maybe-change-cursor))
